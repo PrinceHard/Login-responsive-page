e53b670024d51b70dda0ea33848c3f2fa5d31261
Pedro Henrique <coc.cr.ph@gmail.com>
2021-01-17 15:44:12 -0300

    script-button

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('teste...')
+const signUp = document.getElementById('sign-up')
+const signIn = document.getElementById('sign-in')
+const loginIn = document.getElementById('login-in')
+const loginUp = document.getElementById('login-up')
+
+signUp.addEventListener('click', () => {
+
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
+
+    loginIn.classList.add('none')
+    loginUp.classList.add('block')
+})
+
+signIn.addEventListener('click', () => {
+
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
+
+    loginIn.classList.add('block')
+    loginUp.classList.add('none')
+})
